00cbb1dbb04a5c987983d4fe4e1d8ebd4654516e
renancorporato <117202265+renancorporato@users.noreply.github.com>
2025-09-16 18:04:52 -0300

    feat: function leitura de arquivo

--- scripts.js
+++ scripts.js
@@ -1,6 +1,20 @@
 const uploadBtn = document.getElementById("upload-btn");
-const inputUpload = document.getElementById("image-upload")
+const inputUpload = document.getElementById("image-upload");
 
 uploadBtn.addEventListener("click", () => {
-    inputUpload.click();
-})
+  inputUpload.click();
+});
+
+function letConteudoDoArquivo(arquivo) {
+  return new Promise((resolve, reject) => {
+    const leitor = new FileReader();
+    leitor.onload = () => {
+      resolve({ url: leitor.result, nome: arquivo.name });
+    };
+    leitor.onerror = () => {
+      reject(`Erro na leitura do arquivo ${arquivo.name}`);
+    };
+
+    leitor.readAsDataURL(arquivo);
+  });
+}
